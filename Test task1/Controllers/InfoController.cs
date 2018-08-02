097a5e6753c94226916dcc4a423afe9e059b8cfd
Mikhail Ulyanov <m.ulyanov96@mail.ru>
2018-08-02 15:09:42 +0300

    first commit

---
+++ Test task1/Controllers/InfoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Test_task1.Controllers
+{
+    [Route("api/[controller]")]
+    public class InfoController : Controller
+    {
+        // GET: api/info/fromfile
+        [HttpGet("fromfile")]
+        public string Get()
+        {
+            string res = "";
+            try
+            {
+                StreamReader myReader = new StreamReader(new FileStream("assets/text.txt", FileMode.Open, FileAccess.Read));
+                res = myReader.ReadLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            return res;
+        }
+    }
+}
